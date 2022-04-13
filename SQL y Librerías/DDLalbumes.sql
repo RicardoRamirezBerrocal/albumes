859b8eda04b94d04302ff0926f49a2b0bff8f71d
Ricardo Ramírez Berrocal <91874550+RicardoRamirezBerrocal@users.noreply.github.com>
2022-04-13 09:20:40 +0200

    SQL y Librerías
    Subo el esquema DDL para la creación de la base de datos y la librería usada para leer la base de datos en el programa Java

---
+++ SQL y Librerías/DDLalbumes.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE gestion_albumes;
+
+USE gestion_albumes;
+
+CREATE TABLE albumes (
+    alb_codalb INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    alb_anyo INT UNSIGNED NOT NULL,
+    alb_titulo VARCHAR(50) NOT NULL,
+    alb_artista VARCHAR(50) NOT NULL,
+    alb_tipo VARCHAR(50) NOT NULL,
+    alb_estrellas DOUBLE UNSIGNED NOT NULL,
+    alb_ediciones INT UNSIGNED NOT NULL
+);
